5313b03cc01806ec58bc0c6c6711279f9e6f8d01
asad_ullah_khan <au29072005@gmail.com>
2024-06-20 11:43:03 +0500

    Add files via upload

---
+++ palindromicnumberpattern.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int length;
+    cout<<"Enter the highest no. of the last row: ";
+    cin>>length;
+    
+    for (int i = 1; i <= length; i++)
+    {
+        for (int j = length; j > i; j--)
+        {
+            cout<<"  ";
+        }
+  
+        for (int j = i; j >= 1; j--)
+            {
+                if (j>1)
+                {
+                    cout<<j<<" ";
+                }
+                else{
+                    for (int j = 1; j <= i; j++)
+                    {
+                        cout<<j<<" ";
+                    }
+                    
+                }
+                
+            }
+            
+         cout<<endl;
+    }
+    
+
+    return 0;
+}
